a75988af3e7642f0dfefb803a4a843e85574a9d7
Ivana Lieska <idrabikova@gmail.com>
2016-07-27 12:59:09 +0200

    Lesson 3: Frog Jump
    https://codility.com/demo/results/trainingJVYBJU-9A8/

---
+++ CodilityProblems/Lesson3/FrogJmp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProblems.Lesson3
+{
+    class FrogJmp
+    {
+        //public static void Main(String[] args)
+        //{
+        //    Solution(10, 85, 30);
+        //}
+
+        private static int Solution(int X, int Y, int D)
+        {
+            double x = (double)(Y - X) / D;
+            int y = (int)Math.Ceiling(x);
+            return y;
+        }
+
+    }
+}
+// results: https://codility.com/demo/results/trainingJVYBJU-9A8/
