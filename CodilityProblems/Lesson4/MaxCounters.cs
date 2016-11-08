691fba4e7cff8b5d17f29477a08b238dc615f98d
Ivana Lieska <idrabikova@gmail.com>
2016-11-08 23:23:34 +0100

    lesson 4: missing integer :cactus:

---
+++ CodilityProblems/Lesson4/MaxCounters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProblems.Lesson4
+{
+    class MaxCounters
+    {
+        public int[] Solution(int N, int[] A)
+        {
+            int[] counters = new int[N];
+            int max = 0;
+
+            for (int i = 0; i < counters.Length; i++)
+            {
+                counters[i] = 0;
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] >= 1 && A[i] <= N)
+                {
+                    counters[A[i] - 1]++;
+                    if (counters[A[i] - 1] > max)
+                    {
+                        max = counters[A[i] - 1];
+                    }
+                }
+                if (A[i] == N + 1)
+                {
+                    for (int j = 0; j < counters.Length; j++)
+                    {
+                        counters[j] = max;
+                    }
+                }
+            }
+            return counters;
+        }
+    }
+}
+// https://codility.com/demo/results/trainingVMTB9B-CV3/
