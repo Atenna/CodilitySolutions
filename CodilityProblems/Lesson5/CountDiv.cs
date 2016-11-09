a3fbfa5b1c7ff5c14ef8906c40056b6b9b7a0c22
Ivana Lieska <idrabikova@gmail.com>
2016-11-09 16:51:49 +0100

    Lesson 5: Count divisible integers

---
+++ CodilityProblems/Lesson5/CountDiv.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProblems.Lesson5
+{
+    class CountDiv
+    {
+        public static int Solution(int A, int B, int K)
+        {
+            int count = 0;
+            int iterator = A;
+
+            while (iterator <= B)
+            {
+                if (iterator%K == 0)
+                {
+                    count++;
+                    count += (B-iterator)/K;
+                }
+                else
+                {
+                    iterator++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
+// originally I coded this one in java, but basically, there's no difference in codes
+// https://codility.com/demo/results/trainingMQCJH6-RY3/
